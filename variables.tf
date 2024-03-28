c0e684bc83082253aff2452d889dc8a1b14afb56
Nathalia Paulon <nathaliapaulon@hotmail.com>
2024-03-27 22:34:08 -0300

    Update 22h33

--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "ssh_public_key" {}
+variable "docker_image_name" {
+ description = "Nome da imagem Docker"
+ type        = string
+}
+
+variable "aws_access_key" {
+ description = "AWS Key Acesso"
+ type        = string
+}
+
+variable "aws_secret_key" {
+ description = "AWS Key Secreta"
+ type        = string
+}
+
+variable "ssh_public_key" {
+ description = "SSH Key Publica"
+ type        = string
+}
